7d5532cfb58908b64b0da8c52d6bbfd17cf8fd9e
Radu Toev <radutoev@gmail.com>
2017-01-14 23:08:35 +0200

    box name prefixes

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     loaders: [
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=draftJsHashtagPlugin__[local]__[hash:base64:5]!postcss-loader'),
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=draftJsBoxPlugin__[local]__[hash:base64:5]!postcss-loader'),
       },
     ],
   },
